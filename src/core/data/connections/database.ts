37dd71cd21b48c7e5daf293d70a8f61c1d549045
José Krenzke <krenzkejose@gmail.com.br>
2021-08-12 23:43:07 -0300

    digitação server

--- src/core/data/connections/database.ts
+++ src/core/data/connections/database.ts
@@ -1,4 +1,4 @@
-import {Connection, createConnection} from "typeorm";
+import {Connection, createConnection} from 'typeorm';
 
 export default class Database {
     private static connection: Connection;
@@ -6,19 +6,18 @@ export default class Database {
     public getConnection(): Connection {
         
         if (Database.connection == null || Database.connection undefined) {
-            throw new Error("CONEXAO_DATABASE_NAO_ABERTA");
-        }
-
+            throw new Error('CONEXAO_DATABASE_NAO_ABERTA');
+        
         return Database.connection;
     }
-
+};
     public async openConnection(): Promise<void> {
         if (Database.connection == null || Database.connection === undefined) {
             try {
                 Database.connection-await createConnection();
             }catch (error) {
-                console.log("ERRO AO CONECTAR NO BANCO, error);
-            }
-        }
-    }
-}
+                console.log('ERRO AO CONECTAR NO BANCO, error');
+            };
+        };
+    };
+};
