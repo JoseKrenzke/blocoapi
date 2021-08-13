762f13b2875e943773028805896ce0ca6796cd14
José Krenzke <krenzkejose@gmail.com.br>
2021-08-12 23:07:43 -0300

    Subindo avaliação do banco de dados

---
+++ src/core/data/connections/server.ts
@@ -0,0 +1,18 @@
+import express,{ Request, Response} from 'express';
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+import Database from './core/data/connections/Database';
+
+const app = express();
+const port = process.env.PORT || 8080
+        
+    app.use(express.json());      
+    app.use(express.urlencoded({ extended: false }));
+    
+dotenv.config{{ 
+    path: "../.env"
+    
+    app get('/, (request: Request, response: Response) - { return response.send('API OK);
+    new Database().openConnection().then(() {
+    app listen(port, () { console.log( API Rodando...");
+    });
