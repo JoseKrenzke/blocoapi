51c7118ca5920dd5f01700eece224279bb722761
José Krenzke <krenzkejose@gmail.com.br>
2021-08-12 23:35:16 -0300

    CORRIGINDO DIGITAÇÃO

--- src/core/data/connections/server.ts
+++ src/core/data/connections/server.ts
@@ -1,18 +1,24 @@
-import express,{ Request, Response} from 'express';
+import express, { Request, Response} from 'express';
 import 'reflect-metadata';
 import dotenv from 'dotenv';
 import Database from './core/data/connections/Database';
 
 const app = express();
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
         
     app.use(express.json());      
     app.use(express.urlencoded({ extended: false }));
     
-dotenv.config{{ 
-    path: "../.env"
+dotenv.config({ 
+    path: '../.env'
+});
     
-    app get('/, (request: Request, response: Response) - { return response.send('API OK);
-    new Database().openConnection().then(() {
-    app listen(port, () { console.log( API Rodando...");
+app.get('./', (request: Request, response: Response) => {
+    return response.send('API OK');
+});
+    
+new Database().openConnection().then(() => {
+    app.listen(port, () => {
+        console.log('API Rodando...'),
     });
+});
